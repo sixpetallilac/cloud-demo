9ccae6d8f554b2259cbbe559238c3569e7d0d000
the six-petal lilac <treecheese@163.com>
2024-04-15 19:25:18 +0800

    test

---
+++ order-service/src/main/java/cn/itcast/order/configur/OpinionClass.java
@@ -0,0 +1,17 @@
+package cn.itcast.order.configur;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootConfiguration
+public class OpinionClass {
+    /**
+     * 创建restTemplate注入spring容器
+     * @return
+     */
+    @Bean
+    public RestTemplate restTemplate (){
+        return new RestTemplate();
+    }
+}
